38d55f72f42c24ac923ced0bbfbf2e87862b9c20
ColdMagi <alexsky99@skiff.com>
2022-08-21 19:37:53 +0300

    serverless with express

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,21 @@
 import express from "express";
 import bodyParser from "body-parser";
 import axios from "axios";
+import serverless from "serverless-http";
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 const port = 5000;
+
 app.get("/entry/likers/", async (req, res) => {
   const entryId = req.query.id;
   const likers = await axios
     .get(`https://tjournal.ru/vote/get_likers?id=${entryId}&type=1&mode=raw`)
     .then((resp) => resp.data);
-  res.send(likers);
+  res.json(likers);
 });
-app.listen(port, () => console.log(`Running on port ${port}`));
+// app.listen(port, () => console.log(`Running on port ${port}`));
+
+module.exports.handler = serverless(app);
