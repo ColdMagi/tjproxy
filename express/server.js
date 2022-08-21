a780a6ee6fdfe93f55bcdb4e7781c62057f7fc25
ColdMagi <alexsky99@skiff.com>
2022-08-21 20:31:36 +0300

    redirect to index html if none

--- express/server.js
+++ express/server.js
@@ -36,6 +36,7 @@ router.get("/entry/likers/", async (req, res) => {
 });
 
 app.use("/.netlify/functions/server", router); // path must route to lambda
+app.use("/", (req, res) => res.sendFile(path.join(__dirname, "../index.html")));
 
 module.exports = app;
 module.exports.handler = serverless(app);
