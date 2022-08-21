76b16e4a189b54d7989161aa02f1d7014b23ea66
ColdMagi <alexsky99@skiff.com>
2022-08-21 20:34:27 +0300

    import path

--- express/server.js
+++ express/server.js
@@ -1,5 +1,6 @@
 "use strict";
 const express = require("express");
+const path = require("path");
 const bodyParser = require("body-parser");
 const axios = require("axios");
 const serverless = require("serverless-http");
