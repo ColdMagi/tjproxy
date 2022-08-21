6416f6090b057a4e17acc677b809810b0c14647e
ColdMagi <alexsky99@skiff.com>
2022-08-21 19:08:48 +0300

    return only data from request

--- src/index.ts
+++ src/index.ts
@@ -9,10 +9,9 @@ app.use(bodyParser.json());
 const port = 5000;
 app.get("/entry/likers/", async (req, res) => {
   const entryId = req.query.id;
-  const likers = await axios.get(
-    `https://tjournal.ru/vote/get_likers?id=${entryId}&type=1&mode=raw`
-  );
-  console.log(likers);
+  const likers = await axios
+    .get(`https://tjournal.ru/vote/get_likers?id=${entryId}&type=1&mode=raw`)
+    .then((resp) => resp.data);
   res.send(likers);
 });
 app.listen(port, () => console.log(`Running on port ${port}`));
